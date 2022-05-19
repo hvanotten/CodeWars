8ca50d3ff098fa683c2f4d04fe73a8c4672f50ff
Hillary <hillaryvanotten@gmail.com>
2022-05-19 00:37:26 -0700

    Daily Practice

---
+++ multiply.js
@@ -0,0 +1,12 @@
+/* This code does not execute properly. Try to figure out why.*/
+
+
+// function multiply(a, b) {
+//   a * b;
+// }
+
+// there is no return statement function should be as follows:
+
+function multiply(a, b) {
+    return a * b;
+}
